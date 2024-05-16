140550058a6fbbadf8b1f892ac390066dfac830d
Ayza987 <denise.ndongo@2027.ucac-icam.com>
2024-05-16 17:25:17 +0100

    Project initialization

---
+++ recruiter-back/app/Http/Api/PersonnelController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Http\Api;
+
+use App\Models\Personnel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+
+
+class PersonnelController extends Controller
+{
+    public function index(){
+        $personnels = Personnel::all();
+        if ($personnels->count() > 0){
+
+        return response()->json([
+            'personnels' => $personnels
+
+
+        ], 200);
+    }else{
+        return response()->json([
+            'message' => 'No data found'
+
+
+        ], 404);
+    
+    }
+
+    }
+
+    public function store(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'nom' => 'required|string',
+            'prenom' => 'required|string',
+            'poste' => 'required|string',
+            'email' => 'required|email',
+            'telephone' => 'required|string',
+            'password' => 'required|string',
+            'statut' => 'required|string'
+
+        ]);
+
+        if ($validator->fails()){
+
+            return response()->json([
+                'errors' => $validator->messages()
+
+            ], 422);
+        }else{
+
+            $personnel = Personnel::create([
+                'nom' => $request->nom,
+                'prenom' => $request->prenom,
+                'poste' => $request->poste,
+                'email' => $request->email,
+                'telephone' => $request->telephone,
+                'password' => $request->password,
+                'statut' => $request->statut
+
+            ]);
+
+            if ($personnel){
+
+                return response()->json([
+                    'message' => 'Personnel added'
+        
+        
+                ], 200);
+            }else{
+                return response()->json([
+                    'message' => 'Error sending data'
+
+                ], 500);
+            
+            }
+
+        }
+    }
+
+    public function show($id){
+
+        $personnel = Personnel::find($id);
+        if($personnel){
+            return response()->json([
+                'personnel' => $personnel
+    
+    
+            ], 200);
+
+        }else{
+            return response()->json([
+                'message' => 'No personnel found'
+    
+    
+            ], 404);
+
+        }
+         
+
+    }
+
+    public function update(Request $request, int $id){
+
+        $personnel = Personnel::find($id);
+        if($personnel){
+            $personnel->update([
+                'nom' => $request->nom,
+                'prenom' => $request->prenom,
+                'poste' => $request->poste,
+                'email' => $request->email,
+                'telephone' => $request->telephone,
+                'password' => $request->password,
+                'statut' => $request->statut
+
+            ]);
+            return response()->json([
+                'message' => 'Personnel updated' ], 200);
+
+        }else{
+            return response()->json([
+                'message' => 'No personnel found'
+    
+    
+            ], 404);
+
+        }
+    }
+
+    public function destroy($id){
+
+        $personnel = Personnel::find($id);{
+            if($personnel){
+                $personnel->delete();
+                return response()->json([
+                    'message' => 'Personnel deleted' ], 200);
+            }else{
+                return response()->json([
+                    'message' => 'No personnel found'
+        
+        
+                ], 404);
+            }
+
+        }
+    }
+}
--- recruiter-back/routes/web.php
+++ recruiter-back/routes/web.php
@@ -1,7 +1,14 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Api\PersonnelController;
 
-Route::get('/', function () {
+Route::get('/form', function () {
     return view('welcome');
 });
+
+Route::get('personnel', [PersonnelController::class, 'index']);
+Route::post('personnel', [PersonnelController::class, 'store']);
+Route::get('personnel/{id}',[PersonnelController::class, 'show']);
+Route::put('personnel/{id}/edit', [PersonnelController::class, 'update']);
+Route::delete('personnel/{id}/delete', [PersonnelController::class, 'destroy']);

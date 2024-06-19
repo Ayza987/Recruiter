9f2d56ec5b66bd1609844116d966a1eb13c7ed92
Ayza987 <denise.ndongo@2027.ucac-icam.com>
2024-06-19 14:36:29 +0100

    Code completion

--- recruiter-back/app/Http/Controllers/FileUploadController.php
+++ recruiter-back/app/Http/Controllers/FileUploadController.php
@@ -13,15 +13,31 @@ class FileUploadController extends Controller
         $diplomes = $request->file('diplomes');
 
         if ($cv) {
-            $cv->move('uploads/cv');
+            $this->storeFile($cv, 'uploads/cv');
         }
         if ($lettre_motivation) {
-            $lettre_motivation->move('uploads/lettre_motivation');
+            $this->storeFile($lettre_motivation, 'uploads/lettre_motivation');
         }
         if ($diplomes) {
-            $diplomes->move('uploads/diplomes');
+            $this->storeFile($diplomes, 'uploads/diplomes');
         }
 
         return response()->json(['message' => 'Fichiers téléchargés avec succès']);
+    }
+
+    private function storeFile($file, $destinationPath)
+    {
+        // Récupérer le nom d'origine et l'extension du fichier
+        $originalName = $file->getClientOriginalName();
+        $extension = $file->getClientOriginalExtension();
+        $filename = pathinfo($originalName, PATHINFO_FILENAME) . '.' . $extension;
+
+        // Assurez-vous que le dossier de destination existe
+        if (!file_exists(public_path($destinationPath))) {
+            mkdir(public_path($destinationPath), 0755, true);
+        }
+
+        // Déplacer le fichier vers le dossier de destination avec son nom et son extension d'origine
+        $file->move(public_path($destinationPath), $filename);
     }
 }
